e9123cd9264b482c93d970af3a5951ec908b2d82
DenDen047 <sh.mn.nat@gmail.com>
2022-12-28 22:47:23 +0200

    success to build the docker container

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # init
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
 project=$(cd ~/Documents/Projects/smalr_online && pwd)
 
 # run
 run_cmd="python main.py"
 
-docker build -t denden047/smalr_online docker && \
+docker build -t denden047/smalr_online . && \
 docker run -it --rm \
     -v="${project}:/work" \
     -w="/work/src" \
     denden047/smalr_online \
-    ${run_cmd}
+    /bin/bash
+    # ${run_cmd}
